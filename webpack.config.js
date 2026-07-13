8979f456b32337ef0a97d026be52cd04557ea182
sadshepp <1437877853@qq.com>
2026-07-13 20:41:51 +0800

    将 webpack 开发代理目标 target 变更为 127.0.0.1 避开 Windows 下 localhost IPV6 解析造成的拒绝连接错误

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
     proxy: [
       {
         context: ['/api'],
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true,
       }
     ],
